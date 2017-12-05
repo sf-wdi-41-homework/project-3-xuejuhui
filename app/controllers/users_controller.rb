5c3a81d6147982ef6130a997d2bdb13621752eb3
Ray <xuejuhui@hotmail.com>
2017-12-05 15:42:33 -0800

    init commit and finish auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end 
+	def new 
+		@user = User.new
+	end 
+	def create 
+		@user = User.new(user_params)
+		if @user.save
+			login(@user)
+			redirect_to user_path(@user)
+		else
+			flash[:error] = @user.errors.full_messages.join(' ')
+			redirect_to new_user_path
+		end 
+	end 
+	def show
+		@user = User.find(session[:user_id])
+	end 
+	private
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+	end 
+end
